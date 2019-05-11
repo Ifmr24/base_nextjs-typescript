acdaa110586a8267b079058e092f890a32a862a5
Fabian Mesias <43100897+Ifmr24@users.noreply.github.com>
2019-05-10 22:44:22 -0400

    Update server.ts

--- server.ts
+++ server.ts
@@ -1,33 +1,24 @@
-const express = require('express')
-const next = require('next')
+const express = require("express");
+const next = require("next");
 
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
+const port = process.env.PORT || 3000;
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
 
-app.prepare()
-.then(() => {
-  const server = express()
-  
-  server.get('*', (req:any, res:any) => {
-    return handle(req, res)
-  })
-  
-  /*
-  server.get("/pagina/:id", (req:any, res:any) => {
-    return app.render(req, res, "/pagina", { id: req.params.id });
-  });
-  */
+app.prepare().then(() => {
+    const server = express();
+/*
+    server.get("/profile/:id", (req: { params: { id: any; }; }, res: any) => {
+        return app.render(req, res, "/profile", { id: req.params.id });
+    });
+*/
+    server.get("*", (req: any, res: any) => {
+        return handle(req, res);
+    });
 
-
-
-
-  server.listen(process.env.PORT || 3000, (err:any) => {
-    if (err) throw err
-    console.log(`> Ready on http://localhost:${process.env.PORT || 3000}`)
-  })
-})
-.catch((ex:any) => {
-  console.error(ex.stack)
-  process.exit(1)
-})
+    server.listen(port, (err: any) => {
+        if (err) throw err;
+        console.log(`> Readyss on http://localhost:${port}`);
+    });
+});
